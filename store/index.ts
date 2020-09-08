fe692e6540cdd69fb832c0b7931160499571e4d0
haewon <30405705+wooheet@users.noreply.github.com>
2020-09-08 10:44:55 +0900

    Edit default color

--- store/index.ts
+++ store/index.ts
@@ -12,7 +12,7 @@ export const mutations: any = {
 export const actions = {
     async nuxtServerInit({ commit, dispatch }: any) {
         await commit('modules/yggdrash/SET_CONNECTION', { 
-            network: 'YEED Testnet',
+            network: '매장 찾기',
             url: 'http://testnode.yggdrash.io',
             activeIndex: 0
         })
